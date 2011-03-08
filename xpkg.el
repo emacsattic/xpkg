27ef92386d12c9980c0b56e35dd4406b46ea6b96
Jonas Bernoulli <jonas@bernoul.li>
2011-03-08 21:24:29 +0100

    add simplified variants of things removed from elx

--- xpkg.el
+++ xpkg.el
@@ -43,13 +43,12 @@
 	 (fetcher (plist-get config :fetcher))
 	 (features (xpkg-features ref config nil t))
 	 (hard-deps (nth 1 features))
-	 (soft-deps (nth 2 features))
-	 (mainfile (xpkg-mainfile ref config)))
-    (elx-with-file (cons ref mainfile)
+	 (soft-deps (nth 2 features)))
+    (xpkg-with-file ref (xpkg-mainfile ref config)
       (let ((wikipage (or (let ((page (plist-get config :wikipage)))
 			    (when page
 			      (concat "http://www.emacswiki.org/" page)))
-			  (elx-wikipage mainfile name nil t))))
+			  (elx-wikipage nil name nil t))))
 	(list :summary (elx-summary nil t)
 	      :repository (when (memq fetcher '(bzr cvs darcs git hg svn))
 			    (cons fetcher (plist-get config :url)))
@@ -64,17 +63,24 @@
 			  (if soft-deps
 			      (list hard-deps soft-deps)
 			    (list hard-deps)))
-	      :keywords (elx-keywords mainfile t)
+	      :keywords (elx-keywords nil t)
 	      :homepage (or (elx-homepage)
 			    (plist-get config :homepage)
 			    (when (eq 'wiki (plist-get config :fetcher))
 			      wikipage))
 	      :wikipage wikipage
 	      :commentary (unless (plist-get config :bad-encoding)
-			    (elx-commentary mainfile)))))))
+			    (elx-commentary)))))))
 
 
 ;;; Files.
+
+(defun xpkg-elisp-files (rev)
+  (mapcan (lambda (file)
+	    (when (and (string-match "\\.el\\(\\.in\\)?$" file)
+		       (not (string-match "\\(\\`\\|/\\)\\." file)))
+	      (list file)))
+	  (magit-git-lines "ls-tree" "-r" "--name-only" rev)))
 
 (defun xpkg-mainfile (ref config)
   "Return the mainfile of REF in the current git repository.
@@ -87,7 +93,7 @@ is ignored.  If there is still no match try to extract the value of
 `:mainfile' from the plist CONFIG."
   (let ((name (plist-get config :name))
 	(explicit (plist-get config :mainfile))
-	(files (elx-elisp-files (cons default-directory ref))))
+	(files (xpkg-elisp-files ref)))
     (if (= 1 (length files))
 	(car files)
       (flet ((match (feature)
@@ -101,6 +107,19 @@ is ignored.  If there is still no match try to extract the value of
 	    (when (or (not files) (member explicit files))
 	      explicit))))))
 
+(defmacro xpkg-with-file (rev file &rest body)
+  (declare (indent 2) (debug t))
+  (let ((fsymb (gensym "file"))
+	(rsymb (gensym "rev")))
+    `(let ((,fsymb ,file)
+	   (,rsymb ,rev))
+       (with-temp-buffer
+	 (magit-git-insert (list "show" (format "%s:%s" ,rsymb ,fsymb)))
+	 (goto-char (point-min))
+	 (archive-set-buffer-as-visiting-file ,fsymb)
+	 (setq buffer-file-name ,fsymb)
+	 (with-syntax-table emacs-lisp-mode-syntax-table
+	   ,@body)))))
 
 
 ;;; Keywords.
@@ -127,7 +146,7 @@ PACKAGES is a list of the form ((NAME REPO REF CONFIG)...)."
 Return a sorted list of the provided keywords.  If optional ASSOCIATE is
 non-nil associate the package with the the defined keywords in the value
 of variable `xpkg-keyword-alist'."
-  (let ((keywords (elx-with-file (cons ref (xpkg-mainfile ref config))
+  (let ((keywords (xpkg-with-file ref (xpkg-mainfile ref config)
 		    (elx-keywords))))
     (if (not associate)
 	keywords
@@ -144,25 +163,12 @@ of variable `xpkg-keyword-alist'."
 
 ;;; Features.
 
+;; TODO again add function to update this
 (defconst xpkg-emacs-features (bound-and-true-p xpkg-emacs-features)
   "List of features provided by Emacs.
 
 This variable should be set and saved when a new version of GNU Emacs is
 being targeted and then remain constant.")
-
-(defun xpkg-initialize-emacs-features (&optional directory)
-  "Initialize the value of `xpkg-emacs-features'.
-
-Optional DIRECTORY is the directory where the lisp files of the Emacs
-installation for which you want `xpkg-emacs-features' to be set is
-located, if it is nil or not provided is the lisp directory of the
-running Emacs instance."
-  (interactive (list (read-directory-name "Lisp directory: ")))
-  (with-no-warnings
-    (setq xpkg-emacs-features
-	  (elx-provided
-	   (or directory
-	       (file-name-directory (find-library-name "version")))))))
 
 (defvar xpkg-feature-alist nil
   "Alist of known features and the providing packages.
@@ -225,47 +231,47 @@ Also see the source comments of this function for more information."
 	 (exclude-provided (plist-get config :exclude-provided))
 	 (exclude-path     (plist-get config :exclude-path))
 	 provided required bundled)
-    (dolist (file (elx-elisp-files-git (cons default-directory ref)))
+    (dolist (file (xpkg-elisp-files ref))
       ;; Files that match `exclude-path' are ignored completely; neither
       ;; the provided nor required features appear anywhere in the return
       ;; value of this function at all.
       (unless (and exclude-path (string-match exclude-path file))
-	(dolist (prov (elx-with-file (cons ref file) (elx--buffer-provided)))
-	  ;; If a package bundles required dependencies we do not want
-	  ;; these features to appear in the list of provided features so
-	  ;; that other packages do not pull in these packages instead of
-	  ;; the real upstream package.
-	  ;;
-	  ;; We do this even if the upstream package is not mirrored yet;
-	  ;; which means that other packages depending on features from
-	  ;; the upstream package will have unresolved dependencies,  but
-	  ;; that is still better than instead depending on the current
-	  ;; package whose purpose might be something completely different
-	  ;; but just happens to provide a common dependency.  However for
-	  ;; the current package these features are not considered to be
-	  ;; missing.
-	  ;;
-	  ;; If a bundled feature is not provided by any mirrored package
-	  ;; that feature is neither listed as required nor a provided
-	  ;; feature of this package in the output of this function.
-	  ;;
-	  ;; If a bundled feature is also provided by the mirrored
-	  ;; upstream package that package along with the feature shows up
-	  ;; in the list of required packages.
-	  (if (member prov exclude-provided)
-	      (add-to-list 'bundled prov)
-	    (when prov
-	      (add-to-list 'provided prov))
-	    (when dependencies
-	      ;; Even if some of the features provided by this file are
-	      ;; excluded do not exclude the required features.  We do
-	      ;; this because the file might be legitimately belong to the
-	      ;; package but might never-the-less illegitimately provide
-	      ;; a foreign feature to indicate that is a drop-in
-	      ;; replacement or whatever.
-	      (push (elx-with-file (cons ref file) (elx--buffer-required))
-		    required))))))
-    (setq provided (elx--sanitize-provided provided t))
+	(xpkg-with-file ref file
+	  (dolist (prov (xpkg--provided))
+	    ;; If a package bundles required dependencies we do not want
+	    ;; these features to appear in the list of provided features so
+	    ;; that other packages do not pull in these packages instead of
+	    ;; the real upstream package.
+	    ;;
+	    ;; We do this even if the upstream package is not mirrored yet;
+	    ;; which means that other packages depending on features from
+	    ;; the upstream package will have unresolved dependencies, but
+	    ;; that is still better than instead depending on the current
+	    ;; package whose purpose might be something completely different
+	    ;; but just happens to provide a common dependency.  However for
+	    ;; the current package these features are not considered to be
+	    ;; missing.
+	    ;;
+	    ;; If a bundled feature is not provided by any mirrored package
+	    ;; that feature is neither listed as required nor a provided
+	    ;; feature of this package in the output of this function.
+	    ;;
+	    ;; If a bundled feature is also provided by the mirrored
+	    ;; upstream package that package along with the feature shows up
+	    ;; in the list of required packages.
+	    (if (member prov exclude-provided)
+		(add-to-list 'bundled prov)
+	      (when prov
+		(add-to-list 'provided prov))
+	      (when dependencies
+		;; Even if some of the features provided by this file are
+		;; excluded do not exclude the required features.  We do
+		;; this because the file might be legitimately belong to the
+		;; package but might never-the-less illegitimately provide
+		;; a foreign feature to indicate that is a drop-in
+		;; replacement or whatever.
+		(push (xpkg--required) required)))))))
+    (setq provided (xpkg--sanitize-provided provided))
     (when associate
       ;; If and only if optional argument ASSOCIATE is non-nil add
       ;; associations for the provided features to the value of variable
@@ -309,31 +315,20 @@ Also see the source comments of this function for more information."
 	;; latter so it is possible to skip the step of determining the
 	;; dependencies.
 	provided
-      (setq required (elx--sanitize-required required provided t))
+      (setq required (xpkg--sanitize-required required provided))
       (list provided
-	    (xpkg-lookup-required name ref 'hard (nth 0 required)
-				  exclude-required)
-	    (xpkg-lookup-required name ref 'soft (nth 1 required)
-				  exclude-required)
+	    (xpkg--lookup-required (nth 0 required) exclude-required name 'hard)
+	    (xpkg--lookup-required (nth 1 required) exclude-required name 'soft)
 	    (sort bundled 'string<)))))
 
-(defun xpkg-lookup-required (name ref type required exclude)
-  "Return the packages providing all features in list REQUIRED.
-
-The returned value has the form: ((PACKAGE FEATURE...)...).  If a feature
-is provided by a package that is part of Emacs PACKAGE is \"emacs\" even
-if the real package is also mirrored.  If the package providing a feature
-is not known PACKAGE is nil and if the feature also is not part of EXCLUDE
-a warning is also shown.  The other arguments are only used for these
-waring messages."
+(defun xpkg--lookup-required (required exclude name type)
   (let (packages)
     (dolist (feature required)
-      (let ((package (xpkg-lookup-required-1 feature exclude)))
+      (let ((package (xpkg--lookup-required-1 feature exclude)))
 	(case package
 	  (:excluded)
 	  (nil
-	   (xpkg-log "%s (%s): %s required %s not available"
-		     name ref type feature))
+	   (xpkg-log "%s: %s required %s not available" name type feature))
 	  (t
 	   (let ((entry (assoc package packages)))
 	     (if entry
@@ -347,17 +342,86 @@ waring messages."
 		   (t (string< a b))))
 	   :key 'car)))
 
-(defun xpkg-lookup-required-1 (feature exclude)
+(defun xpkg--lookup-required-1 (feature exclude)
   "Return a string representing the package that provides FEATURE."
   (cond ((member feature xpkg-emacs-features) "emacs")
 	((member feature exclude) :excluded)
 	((not (string-match "-autoloads?$" (symbol-name feature)))
 	 (cdr (assoc feature xpkg-feature-alist)))
 	(t
-	 (xpkg-lookup-required-1
-	  (intern (substring (symbol-name feature) 0
-			     (match-beginning 0)))
+	 (xpkg--lookup-required-1
+	  (intern (substring (symbol-name feature) 0 (match-beginning 0)))
 	  exclude))))
+
+(defconst xpkg-provided-regexp "\
+\(\\(?:cc-\\|silentcomp-\\)?provide[\s\t\n]+'\
+\\([^(),\s\t\n]+\\)\\(?:[\s\t\n]+'\
+\(\\([^(),]+\\))\\)?)")
+
+(defun xpkg--provided ()
+  (let (features)
+    (save-excursion
+      (goto-char (point-min))
+      (while (re-search-forward xpkg-provided-regexp nil t)
+	(unless (save-match-data
+		  (or (nth 3 (syntax-ppss))   ; in string
+		      (nth 4 (syntax-ppss)))) ; in comment
+	  (dolist (feature (cons (match-string 1)
+				 (when (match-string 2)
+				   (split-string (match-string 2) " " t))))
+	    (add-to-list 'features (intern feature))))))
+    (xpkg--sanitize-provided features)))
+
+(defun xpkg--sanitize-provided (provided)
+  (let (sanitized)
+    (dolist (feature provided)
+      (unless (member feature sanitized)
+	(push feature sanitized)))
+    (sort sanitized #'string<)))
+
+(defconst xpkg-required-regexp "\
+\(\\(?:cc-\\)?require[\s\t\n]+'\
+\\([^(),\s\t\n]+\\)\
+\\(?:\\(?:[\s\t\n]+\\(?:nil\\|\".*\"\\)\\)\
+\\(?:[\s\t\n]+\\(?:nil\\|\\(t\\)\\)\\)?\\)?)")
+
+(defun xpkg--required ()
+  (let (hard soft)
+    (save-excursion
+      (goto-char (point-min))
+      (while (re-search-forward xpkg-required-regexp nil t)
+	(let ((feature (intern (match-string 1))))
+	  (cond ((save-match-data
+		   (or (nth 3 (syntax-ppss))    ; in string
+		       (nth 4 (syntax-ppss))))) ; in comment
+		((match-string 2)
+		 (unless (or (member feature hard)
+			     (member feature soft))
+		   (push feature soft)))
+		((not (member feature hard))
+		 (setq soft (remove feature soft))
+		 (push feature hard))))))
+    (xpkg--sanitize-required (list (list hard soft)))))
+
+(defun xpkg--sanitize-required (required &optional exclude)
+  (let (hard soft)
+    (dolist (requ required)
+      (setq hard (append (nth 0 requ) hard)
+	    soft (append (nth 1 requ) soft)))
+    (setq hard (xpkg--sanitize-required-1 hard exclude)
+	  soft (xpkg--sanitize-required-1 soft (append exclude hard)))
+    (if soft
+	(list hard soft)
+      (when hard
+	(list hard)))))
+
+(defun xpkg--sanitize-required-1 (required &optional exclude)
+  (let (sanitized)
+    (dolist (feature required)
+      (unless (or (member feature exclude)
+		  (member feature sanitized))
+	(push feature sanitized)))
+    (sort sanitized #'string<)))
 
 
 ;;; Utilities.
